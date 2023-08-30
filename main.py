b2011026ec041e43da1ffdea6659b53f2f352735
gluchklaudia <gluch.klaudiaa@gmail.com>
2023-08-30 17:33:20 +0200

    Ask user for required data

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+name = input("Imię zwierzaka: ")
+weight = float(input("Waga: "))
+sex = input("Płeć: ")
+fiv_felv = input("Czy stwierdzono FIV/FeLv? (Tak/Nie): ").upper()
+castration_sterilization = input("Czy kot jest wykastrowany/wysterylizowany?(Tak/Nie): ").upper()
+
+data = {"name": name, "weight": weight, "sex": sex, "fiv_felv": fiv_felv, "castration": castration_sterilization}
+
+print(data)
