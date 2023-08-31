63d32cce57d5aa00778f8e553cde093a4063f468
gluchklaudia <gluch.klaudiaa@gmail.com>
2023-08-31 19:23:24 +0200

    Add function that load data from file

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import json
+
+
+def load_data_from_file():
+    with open("data_file.json", "r") as file:
+        data = json.load(file)
+    return data
+
+data = load_data_from_file()
+
 name = input("Imię zwierzaka: ")
 weight = float(input("Waga: "))
 sex = input("Płeć: ")
 fiv_felv = input("Czy stwierdzono FIV/FeLv? (Tak/Nie): ").upper()
 castration_sterilization = input("Czy kot jest wykastrowany/wysterylizowany?(Tak/Nie): ").upper()
 
-data = {"name": name, "weight": weight, "sex": sex, "fiv_felv": fiv_felv, "castration": castration_sterilization}
+data.append({"name": name, "weight": weight, "sex": sex, "fiv_felv": fiv_felv, "castration": castration_sterilization})
 
-print(data)
+with open("data_file.json", "w") as file:
+    json.dump(data, file)
+
+
+
+
